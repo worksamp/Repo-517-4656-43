658027543e1e6a65e13059f1c796ca8077fd9ab5
Nikita <work.samp01@gmail.com>
2016-12-26 09:21:07 +0000

    ch

---
+++ NUnit_Application/NUnit_Application.Test/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit;
+using NUnit_Application;
+using NUnit.Framework;
+
+namespace NUnit_Application.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(40, result);
+        }
+
+        [TestCase]
+        public void SubtractTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
